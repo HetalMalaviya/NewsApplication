dab7db51fa3440c0c51fe400ab43624e6a0c6773
HetalMalaviya <hetalmalaviya71@gmail.com>
2022-08-18 15:14:29 +0200

    intial commit

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -8,14 +8,19 @@ export class NewsItem extends Component {
           <img className="w-full" src={this.props.imageUrl} alt="" />
 
           <div className="px-6 py-4 max-w-sm rounded overflow-hidden shadow-lg mt-5">
-            <div className="font-bold text-xl mb-2">{this.props.title}</div>
-            <p className="text-gray-700 text-base">{this.props.description}</p>
-            <button
+            <div className="font-bold text-xl mb-2">{this.props.title}...</div>
+            <p className="text-gray-700 text-base">
+              {this.props.description}...
+            </p>
+            <a
+              href={this.props.newsUrl}
+              target="_blank"
               type="button"
+              // onClick={this.fetchNews}
               className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mt-4"
             >
               Read more
-            </button>
+            </a>
           </div>
         </div>
       </>
